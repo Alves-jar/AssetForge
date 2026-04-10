815c77d199c629ae3104ace6d60c7f33cc2238f0
AlvesKZ <igoralvex01@gmail.com>
2026-04-10 16:48:26 -0300

    create project

---
+++ src/main/java/com/noxus/AssetForge/AssetForgeApplication.java
@@ -0,0 +1,13 @@
+package com.noxus.AssetForge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetForgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noxus/AssetForge/AssetForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noxus.AssetForge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
